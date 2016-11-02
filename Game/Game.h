86b62ec8b598c4350aa916c826e9573342569204
Jonah Witcig <jwitcig@gmail.com>
2016-11-02 01:18:51 -0500

    Initial Commit

---
+++ Game/Game.h
@@ -0,0 +1,19 @@
+//
+//  Game.h
+//  Game
+//
+//  Created by Jonah Witcig on 11/2/16.
+//  Copyright © 2016 Jonah Witcig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Game.
+FOUNDATION_EXPORT double GameVersionNumber;
+
+//! Project version string for Game.
+FOUNDATION_EXPORT const unsigned char GameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Game/PublicHeader.h>
+
+
